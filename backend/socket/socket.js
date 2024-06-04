5a8b8c396222d88549057f8d024a21ddbc4b7207
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-06-04 04:00:04 -0700

    new url updatae

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io=  new Server(server,{
     cors: {
-        origin: "https://mern-chat-web.vercel.app",
+        origin: "https://chatdeveloped.netlify.app/",
         methods: ["GET", "POST"],
     }
 }); 
